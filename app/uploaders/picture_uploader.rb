d82b2b5ae2dc69662d1031eea15cf5aa7c48cc6b
robocopkaka <robocopkaka@gmail.com>
2016-02-26 18:53:26 +0100

    chaged values for process_to_fill

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,7 +3,7 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process resize_to_fill: [250, 250]
+  process resize_to_fill: [400, 400]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
