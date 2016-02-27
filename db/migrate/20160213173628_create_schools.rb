501b114de5b54362a10b67aa4c953e148f6be1a5
robocopkaka <robocopkaka@gmail.com>
2016-02-27 16:29:17 +0100

    Added null to create school migration

--- db/migrate/20160213173628_create_schools.rb
+++ db/migrate/20160213173628_create_schools.rb
@@ -7,7 +7,7 @@ class CreateSchools < ActiveRecord::Migration
       t.string :location
       t.string :website
       t.string :picture
-      t.boolean :approved, default: false
+      t.boolean :approved, null: false, default: false
       t.float :latitude
       t.float :longitude
       t.string :classification
